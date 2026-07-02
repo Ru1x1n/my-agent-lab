bc8d2870844ec29921738ac59c896a3db62045c6
Ru1x1n <ama8us@virginia.edu>
2026-07-01 22:14:45 -0400

    Day 1: 完成环境搭建和三个实验

---
+++ day1_hello.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+# 关键：指定 DeepSeek 的地址和密钥
+client = OpenAI(
+    api_key=os.getenv("DEEPSEEK_API_KEY"),
+    base_url="https://api.deepseek.com"
+)
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    max_tokens=300,
+messages=[
+        {"role": "user", "content": "deepseek模型1美元大概多少token？"}
+    ],
+)
+
+print(response.choices[0].message.content)
